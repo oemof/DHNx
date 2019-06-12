0097bff2d4240546459787b54f90540db5cf5714
jnnr <32454596+jnnr@users.noreply.github.com>
2019-06-12 14:09:04 +0200

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='district_heating_simulation',
+      version='0.0.1',
+      description='Simulation of district heating and cooling networks',
+      author='Jann Launer',
+      author_email='jann.launer@rl-institut.de',
+      long_description=read('README.rst'),
+      packages=['district_heating_simulation'],
+      install_requires=[])
