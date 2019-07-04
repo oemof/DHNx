75ad696fb7198496d41ee80db466a6e74f409de5
Johannes Roeder <johannes.roeder@uni-bremen.de>
2019-07-04 10:26:15 +0200

    Initial files

---
+++ geometry_operations.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+General description
+-------------------
+Some functions for connect_points application.
+
+04.07.2019 - johannes.roeder@uni-bremen.de
+
+"""
+
+import geopandas as gpd
+from shapely.geometry import Point
+from shapely import wkt
+
+
+def create_nodes(lines):
+    """
+    :param lines: geopandas.DataFrame with LineStrings of distribution
+                        lines
+    :return:    nodes: geopandas.DataFrames containing all nodes of Line-Layer
+                    with an identifier column 'K123';
+                lines: return line dataframe of input with new columns
+                    'id_start' and 'id_end' holding the ids of the nodes of
+                    start-point and end-point of line;
+    """
+
+    nodes = gpd.GeoDataFrame()
+
+    for i, r in lines.iterrows():
+        line_str = lines.iloc[i]['geometry']
+        p_0 = Point(line_str.coords[0])
+        p_1 = Point(line_str.coords[1])
+        nodes = nodes.append({'geometry': p_0}, ignore_index=True)
+        nodes = nodes.append({'geometry': p_1}, ignore_index=True)
+
+    # drop duplicates
+    # length before deleting douples
+    length_1 = len(nodes)
+    # transform geometry into wkt
+    nodes["geometry"] = nodes["geometry"].apply(lambda geom: geom.wkt)
+    # drop duplicates of geometry column
+    nodes = nodes.drop_duplicates(["geometry"])
+    # create shapely geometry again
+    nodes["geometry"] = nodes["geometry"].apply(
+        lambda geom: wkt.loads(geom))
+    # reset index
+    nodes = nodes.reset_index(drop=True)
+    # print the number of deleted points
+    length_2 = len(nodes)
+    print('Deleted duplicate points:', length_1 - length_2)
+
+    # add a specific id to each node
+    nodes['ind'] = nodes.index
+    nodes['id'] = 'K' + nodes['ind'].apply(str)
+    nodes['type'] = 'K'
+
+    return nodes
+
+
+def insert_node_ids(lines, nodes):
+    """
+
+    :param lines:
+    :param nodes:
+    :return:
+    """
+
+    nodes['geo_wkt'] = nodes["geometry"].apply(lambda geom: geom.wkt)
+
+    # add id to gdf_lines for starting and ending point
+    # point as wkt
+    lines['b0_wkt'] = \
+        lines["geometry"].apply(lambda geom: geom.boundary[0].wkt)
+    lines['b1_wkt'] = \
+        lines["geometry"].apply(lambda geom: geom.boundary[-1].wkt)
+
+    # do kind of vlookup for adding the node-ids to the lines
+    # node1: starting node
+    lines = lines.merge(nodes[['geo_wkt', 'id']], left_on='b0_wkt',
+                        right_on='geo_wkt', suffixes=('', '_start'))
+
+    # delete help columns
+    lines.drop(axis=1, inplace=True, labels=['b0_wkt', 'geo_wkt'])
+
+    # node1: ending node
+    lines = lines.merge(nodes[['geo_wkt', 'id']], left_on='b1_wkt',
+                        right_on='geo_wkt', suffixes=('', '_end'))
+
+    # delete help columns
+    lines.drop(axis=1, inplace=True, labels=['b1_wkt', 'geo_wkt'])
+    nodes.drop(axis=1, inplace=True, labels=['geo_wkt'])
+
+    a = len(lines) - lines['id_start'].count()
+    b = len(lines) - lines['id_end'].count()
+
+    print('Anzahl id_start = NaN: ', a)
+    print('Anzahl id_end = NaN: ', b)
+
+    return lines
