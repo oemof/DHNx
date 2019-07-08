c7823e21e8e5f37f4b4c14f02fe0d40af7d5953c
jnnr <32454596+jnnr@users.noreply.github.com>
2019-07-08 15:59:57 +0200

    Get graph and plot it

--- examples/import_export/import_export_new.py
+++ examples/import_export/import_export_new.py
@@ -1,15 +1,21 @@
 import district_heating_simulation as dhs
-import district_heating_simulation.network as nx
 
 # initialize a thermal network
 thermal_network = dhs.network.ThermalNetwork()
 
 # load data from csv
 thermal_network.load_from_csv('data/')
-print(thermal_network.producers)
 
 # save thermal network to csv
 thermal_network.save_to_csv('data2/')
 
 # save thermal network to GeoDataFrame
 # thermal_network.save_to_gdf('data2/')
+
+# get graph of thermal network
+graph = thermal_network.get_nx_graph()
+
+# plot the graph
+dhs.plotting.draw_G(graph, 5, 5)
+
+
