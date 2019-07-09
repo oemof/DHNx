6e0842b75cef48039c24b8b93f1478d6356a20cf
jlnr <32454596+jnnr@users.noreply.github.com>
2019-07-09 16:23:16 +0200

    Add example for simulations

---
+++ examples/simulation/simulation_example.py
@@ -0,0 +1,16 @@
+import district_heating_simulation as dhs
+import matplotlib.pyplot as plt
+
+# initialize a thermal network
+thermal_network = dhs.network.ThermalNetwork()
+
+# load data from csv
+thermal_network.load_from_csv('data/')
+
+# define problem
+
+# create simulation model
+
+# solve the model
+
+# plot results
