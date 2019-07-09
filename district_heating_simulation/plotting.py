f5a5ddc82a6293651f5e2d850ed134ce238dfc57
jlnr <32454596+jnnr@users.noreply.github.com>
2019-07-09 16:08:00 +0200

    Use cartopy for background map

--- district_heating_simulation/plotting.py
+++ district_heating_simulation/plotting.py
@@ -2,7 +2,8 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.collections as collections
-from cartopy.io.img_tiles import GoogleTiles
+from cartopy.io.img_tiles import Stamen
+from cartopy import crs as ccrs
 
 class InteractiveMap():
     r"""
@@ -44,13 +45,15 @@ class GraphPlot():
         self.edge_color = edge_color
         self.positions = {node_id: np.array([data['lon'], data['lat']])
                           for node_id, data in self.graph.nodes(data=True)}
-        self.extent = self.get_extent()
-        print(self.extent)
+        self.extent = self._get_extent()
 
-    def get_extent(self):
+    def _get_extent(self):
         lon = [pos[0] for pos in self.positions.values()]
         lat = [pos[1] for pos in self.positions.values()]
-        return  np.min(lon), np.max(lon), np.min(lat), np.max(lat)
+        extent = np.array([np.min(lon), np.max(lon), np.min(lat), np.max(lat)])
+        delta = [extent[1] - extent[0], extent[3] - extent[2]]
+        extent += 0.1 * np.array([-delta[0], delta[0], -delta[1], delta[1]])
+        return extent
 
     def draw(self, no_axis=False):
         fig = plt.figure(figsize=self.figsize)
@@ -83,23 +86,22 @@ class GraphPlot():
                edge_alpha=1, node_size=3, node_color='r', node_alpha=1,
                node_edgecolor='r', node_zorder=1):
 
-        G = self.graph
-
         if background_map:
-            imagery = GoogleTiles()
-            zoom_level = 7
+            imagery = Stamen(style='toner-lite')
+            zoom_level = 15
             fig, ax = plt.subplots(figsize=self.figsize,
                                    facecolor=bgcolor,
                                    subplot_kw={'projection': imagery.crs})
-            ax.set_extent(self.extent, crs=imagery.crs)
-            ax.add_image(imagery, zoom_level)
-            ax.plot(12, 52, marker='o', color='red', markersize=12,
-                    alpha=0.7)
+            ax.set_extent(self.extent, crs=ccrs.Geodetic())
+            ax.add_image(imagery, zoom_level, alpha=1, interpolation='bilinear')
+            ax.plot(12.2, 52, marker='o', color='red', markersize=12,
+                    alpha=0.7, transform=ccrs.Geodetic())
+
         else:
             fig, ax = plt.subplots(figsize=self.figsize, facecolor=bgcolor)
 
         lines = []
-        for u, v, data in G.edges(data=True):
+        for u, v, data in self.graph.edges(data=True):
             if 'geometry' in data and use_geom:
                 # if it has a geometry attribute (a list of line segments), add them
                 # to the list of lines to plot
@@ -108,10 +110,10 @@ class GraphPlot():
             else:
                 # if it doesn't have a geometry attribute, the edge is a straight
                 # line from node to node
-                x1 = G.nodes[u]['lon']
-                y1 = G.nodes[u]['lat']
-                x2 = G.nodes[v]['lon']
-                y2 = G.nodes[v]['lat']
+                x1 = self.graph.nodes[u]['lon']
+                y1 = self.graph.nodes[u]['lat']
+                x2 = self.graph.nodes[v]['lon']
+                y2 = self.graph.nodes[v]['lat']
                 line = [(x1, y1), (x2, y2)]
                 lines.append(line)
 
@@ -120,11 +122,12 @@ class GraphPlot():
                                         colors=edge_color,
                                         linewidths=edge_linewidth,
                                         alpha=edge_alpha,
-                                        zorder=2)
+                                        zorder=2,
+                                        transform=ccrs.Geodetic())
         ax.add_collection(lc)
 
-        node_Xs = [float(x) for _, x in G.nodes(data='lon')]
-        node_Ys = [float(y) for _, y in G.nodes(data='lat')]
+        node_Xs = [float(x) for _, x in self.graph.nodes(data='lon')]
+        node_Ys = [float(y) for _, y in self.graph.nodes(data='lat')]
 
         ax.scatter(node_Xs,
                    node_Ys,
@@ -132,7 +135,8 @@ class GraphPlot():
                    c=node_color,
                    alpha=node_alpha,
                    edgecolor=node_edgecolor,
-                   zorder=node_zorder)
+                   zorder=node_zorder,
+                   transform=ccrs.Geodetic())
 
         if no_axis:
             ax = plt.gca()
