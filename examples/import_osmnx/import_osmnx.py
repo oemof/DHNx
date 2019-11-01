03367a225a9b778729c739381645167abb0dc8c2
jnnr <32454596+jnnr@users.noreply.github.com>
2019-11-01 19:26:09 +0100

    Move function to new module

--- examples/import_osmnx/import_osmnx.py
+++ examples/import_osmnx/import_osmnx.py
@@ -3,11 +3,8 @@ import os
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import osmnx as ox
-import pandas as pd
 
 import district_heating_simulation as dhs
-from shapely.ops import nearest_points
-from shapely.geometry import LineString
 
 
 # load street network and footprints from osm
@@ -57,71 +54,12 @@ nodes = nodes.drop('index', 1)
 edges = edges.loc[:, ['u', 'v', 'geometry']]
 edges.loc[:, ['u', 'v']] = edges.loc[:, ['u', 'v']].replace(replace_ids)
 
-
-def connect_points_to_network(points, nodes, edges):
-    r"""
-
-    Parameter
-    ---------
-    points :
-
-    nodes : geopandas.GeoDataFrame
-        Nodes of the network
-
-    edges : geopandas.GeoDataFrame
-        Edges of the network
-
-    Returns
-    -------
-    points :
-    nodes :
-    edges :
-    """
-    edges_united = edges.unary_union
-
-    len_nodes = len(nodes)
-    len_points = len(points)
-
-    # assign ids to new points
-    n_points = []
-    n_nearest_points = []
-    n_edges = []
-
-    for i, point in enumerate(points.geometry):
-        id_nearest_point = len_nodes + i
-
-        id_point = len_nodes + len_points + i
-
-        nearest_point = nearest_points(edges_united, point)[0]
-
-        n_points.append([id_point, point.x, point.y, point])
-
-        n_nearest_points.append([id_nearest_point, nearest_point.x, nearest_point.y, nearest_point])
-
-        n_edges.append([id_point, id_nearest_point, LineString([point, nearest_point])])
-
-    n_points = gpd.GeoDataFrame(
-        n_points,
-        columns=['index', 'x', 'y', 'geometry']).set_index('index')
-
-    n_nearest_points = gpd.GeoDataFrame(
-        n_nearest_points,
-        columns=['index', 'x', 'y', 'geometry']).set_index('index')
-
-    n_edges = gpd.GeoDataFrame(n_edges, columns=['u', 'v', 'geometry'])
-
-    joined_nodes = pd.concat([nodes, n_nearest_points], sort=True)
-    joined_edges = pd.concat([edges, n_edges], sort=True)
-
-    return n_points, joined_nodes, joined_edges
-
-
 building_midpoints = gpd.GeoDataFrame(footprints.geometry.centroid, columns=['geometry'])
 building_midpoints['x'] = building_midpoints.apply(lambda x: x.geometry.x, 1)
 building_midpoints['y'] = building_midpoints.apply(lambda x: x.geometry.y, 1)
 building_midpoints = building_midpoints[['x', 'y', 'geometry']]
 
-points, splits, edges = connect_points_to_network(building_midpoints, nodes, edges)
+points, splits, edges = dhs.dhn_from_osm.connect_points_to_network(building_midpoints, nodes, edges)
 
 producer = points.loc[[323], :]
 consumer = points.drop(323)
