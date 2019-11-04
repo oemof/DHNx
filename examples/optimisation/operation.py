03496f4b045f1956a13a3b7b924d00ad61c11af2
jnnr <32454596+jnnr@users.noreply.github.com>
2019-11-04 18:23:33 +0100

    Add draft for examples

---
+++ examples/optimisation/operation.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import district_heating_simulation as dhs
+
+
+# Initialize thermal network
+network = dhs.network.ThermalNetwork()
+network.load_from_csv('operation_network')
+
+# Draw network
+network_plot = dhs.plotting.GraphPlot(network)
+network_plot.draw_G(background_map=False)
+plt.show()
