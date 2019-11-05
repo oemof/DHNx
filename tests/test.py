c5f7bb128e57f0a7ebea3a409624efe95a40d072
jnnr <32454596+jnnr@users.noreply.github.com>
2019-11-05 15:30:50 +0100

    Add test for optimisation model

--- tests/test.py
+++ tests/test.py
@@ -17,3 +17,20 @@ def test_import_export_csv(tmpdir):
     is_same = filecmp.cmpfiles(dir_import, dir_export, files)
 
     assert is_same
+
+
+def test_operation_optimization():
+    dir_import = '_files/network_import'
+    dir_problem = '_files/problem_operation'
+
+    network = dhs.network.ThermalNetwork()
+    network.load_from_csv(dir_import)
+
+    problem = dhs.input_output.load_problem(dir_problem)
+
+    model = dhs.models.OperationOptimizationModel(network, problem)
+
+    results = model.solve()
+    default_results = None
+
+    assert results == default_results
