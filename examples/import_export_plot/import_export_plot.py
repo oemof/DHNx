722abaeb41e128413e9e136aa8d6680273d1c33c
jnnr <32454596+jnnr@users.noreply.github.com>
2019-11-07 18:34:31 +0100

    Cleanup plotting part of example

--- examples/import_export_plot/import_export_plot.py
+++ examples/import_export_plot/import_export_plot.py
@@ -5,25 +5,18 @@ import matplotlib.pyplot as plt
 thermal_network = dhs.network.ThermalNetwork()
 
 # load data from csv
-thermal_network.load_from_csv('data_csv_input/')
+thermal_network.load_from_csv('data_csv_input')
 
 # save thermal network to csv
-thermal_network.save_to_csv('data_csv_output/')
-
-# save thermal network to GeoDataFrame
-# thermal_network.save_to_gdf('data_gdf/')
+thermal_network.save_to_csv('data_csv_output')
 
 # get graph of thermal network
 graph = thermal_network.get_nx_graph()
 
-# plot the graph
-graph_plot = dhs.plotting.GraphPlot(thermal_network)
-# plot_1 = graph_plot.draw()
-plot_2 = graph_plot.draw_G(background_map=False)
-plt.show()
+# plot static map
+static_map = dhs.plotting.StaticMap(thermal_network)
+static_map.draw(background_map=False)
+plt.savefig('static_map.png')
 
-# # plot static map
-# static_map = dhs.plotting.StaticMap(thermal_network)
-#
-# # plot interactive map
-# interactive_map = dhs.plotting.InteractiveMap(thermal_network)
+# plot interactive map
+interactive_map = dhs.plotting.InteractiveMap(thermal_network)
