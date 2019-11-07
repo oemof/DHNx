d68b44e8530565a8ad30aa1dffb1a73455db1af3
jnnr <32454596+jnnr@users.noreply.github.com>
2019-11-07 19:07:54 +0100

    Adapt example

--- examples/berlin_example/plot_berlin.py
+++ examples/berlin_example/plot_berlin.py
@@ -1,11 +1,14 @@
 import os
 import sys
+
+import pandas as pd
+
+from district_heating_simulation import plotting
+
+
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(os.path.dirname(currentdir))
 sys.path.append(parentdir)
-from district_heating_simulation import plotting
-import pandas as pd
-
 
 # read data
 node_data = pd.read_csv('berlin_node_list.csv')
@@ -17,16 +20,9 @@ place = 'berlin'
 # coordinates of the place
 point = (node_data['lat'].mean(), node_data['lon'].mean())
 
-# create network & plot
-net = plotting.Network(place, point, node_data, edge_data)
-plot = net.draw_map(distance=2500, dpi=70)
-
-# save plot
-plot.savefig('plot_'+place+'.png', dpi=70, facecolor='#333333')
-print('Figure saved as '+place+'.png')
-
 # create interactive map
-map = net.create_interactive_map()
+map = plotting.InteractiveMap(place, point, node_data, edge_data)
+map = map.draw()
 
 # save interactive map
 map.save(place+'.html')
