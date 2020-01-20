de41651b487d6bfbc55cb3a35f4e4a74b9f0a5c6
jnnr <32454596+jnnr@users.noreply.github.com>
2020-01-20 17:00:58 +0100

    Cleanup example

--- examples/tespy_simulation/minimal_example.py
+++ examples/tespy_simulation/minimal_example.py
@@ -9,23 +9,22 @@ from tespy_facades import (
 )
 
 
-# This is a test of a minimal example of a district heating network
+# This is a minimal example of a district heating network
 # TODO: Check if solution is correct
-# TODO: Try solving a network with loops
 # TODO: Setup a builder
+# TODO: Solve a tree-like network
+# TODO: Try solving a network with loops
 
 nw = network(
     fluids=['water'], T_unit='C', p_unit='bar', h_unit='kJ / kg', m_unit='kg / s'
 )
 
 # producer
-# hint might be replaced by: cyclecloser
 heat_producer = HeatProducer('heat_producer')
 heat_producer.comps['heat_exchanger'].set_attr(pr=0.99)
 heat_producer.comps['pump'].set_attr(eta_s=0.8)
 
-
-# consumer (housing area 4)
+# consumer
 consumer_0 = HeatConsumer('consumer_0')
 consumer_0.comps['heat_exchanger'].set_attr(Q=-5e4, pr=0.99)
 consumer_0.comps['valve'].set_attr(pr=1)
@@ -35,13 +34,12 @@ pipe_0 = DistrictHeatingPipe('pipe_0', heat_producer, consumer_0)
 
 nw.add_subsys(heat_producer, consumer_0, pipe_0)
 
+# collect lost and consumed heat
 heat_losses = bus('network losses')
 heat_consumer = bus('network consumer')
 
-# nw.add_conns(*cons)
 nw.check_network()
 
-# set attributes
 for comp in nw.comps.index:
     if isinstance(comp, pipe):
 
@@ -54,9 +52,11 @@ for comp in nw.comps.index:
 
 nw.add_busses(heat_losses, heat_consumer)
 
+# silence warnings
 for comp in nw.comps.index:
     comp.char_warnings = False
 
+# solve
 nw.solve('design')
 nw.save('minimal_example')
 
