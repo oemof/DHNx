e60b919e5703ef9210b8679a183ed2eec60f423f
jnnr <32454596+jnnr@users.noreply.github.com>
2020-01-22 15:17:49 +0100

    Setup tmpdir for tests

---
+++ tests/helpers.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+"""
+This is a collection of helper functions that can be used within the tests.
+
+This file is part of project DHNx (). It's copyrighted
+by the contributors recorded in the version control history of the file,
+available from its original location.
+
+SPDX-License-Identifier: MIT
+"""
+
+import os
+import pandas as pd
+
+
+def get_basic_path():
+    """Returns the basic oemof path and creates it if necessary.
+    The basic path is the '.oemof' folder in the $HOME directory.
+    """
+    basicpath = os.path.join(os.path.expanduser('~'), '.DHNx')
+    if not os.path.isdir(basicpath):
+        os.mkdir(basicpath)
+    return basicpath
+
+
+def extend_basic_path(subfolder):
+    """Returns a path based on the basic oemof path and creates it if
+     necessary. The subfolder is the name of the path extension.
+    """
+    extended_path = os.path.join(get_basic_path(), subfolder)
+    if not os.path.isdir(extended_path):
+        os.mkdir(extended_path)
+    return extended_path
+
+
+def check_if_csv_files_equal(csv_file_a, csv_file_b):
+    df1 = pd.read_csv(csv_file_a)
+    df2 = pd.read_csv(csv_file_b)
+
+    return df1.equals(df2)
+
+
+def check_if_csv_dirs_equal(dir_a, dir_b):
+    files_a = [os.path.join(dir_a, f) for f in os.listdir(dir_a)]
+    files_b = [os.path.join(dir_b, f) for f in os.listdir(dir_b)]
+    files_a.sort()
+    files_b.sort()
+
+    for file_a, file_b in zip(files_a, files_b):
+        assert check_if_csv_files_equal(file_a, file_b)
---
+++ tests/test_io.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+This is a collection of helper functions which work on their own and can be
+used by various classes. If there are too many helper-functions, they will
+be sorted in different modules.
+
+This file is part of project oemof (github.com/oemof/oemof). It's copyrighted
+by the contributors recorded in the version control history of the file,
+available from its original location oemof/oemof/tools/helpers.py
+
+SPDX-License-Identifier: MIT
+"""
+
+import os
+
+import district_heating_simulation as dhs
+import helpers
+
+
+tmpdir = helpers.extend_basic_path('tmp')
+
+
+def test_import_export_csv(tmpdir):
+    basedir = os.path.dirname(__file__)
+    dir_import = os.path.join(basedir, '_files/network_import')
+    dir_export = os.path.join(tmpdir, 'network_export')
+
+    network = dhs.network.ThermalNetwork()
+    network = network.load_from_csv(dir_import)
+
+    network.save_to_csv(dir_export)
+
+    helpers.check_if_csv_dirs_equal(dir_import, dir_export)
