a3b29309d58003da751ace44b6f1c1c102dd1e95
Johannes Roeder <johannes.roeder@uni-bremen.de>
2020-03-16 17:22:20 +0100

    Update and comment example

--- examples/optimisation/investment.py
+++ examples/optimisation/investment.py
@@ -6,20 +6,19 @@ import dhnx
 network = dhnx.network.ThermalNetwork()
 network = network.from_csv_folder('investment_input_2')
 
-# some data -> need to be moved somewhere else
+# general optimisation settings
 num_ts = 2      # number of timesteps
-time_res = 1    # time resolution: [1/h] (percentage of hour)
-# => 0.25 is quarter-hour resolution
-set = {'num_ts': num_ts,  # number of timesteps
-      'time_res': time_res,
-      'rate': 0.01,
-      'f_invest': num_ts / (8760 / time_res),   # just in case annuity is on
-      # 'f_invest': 1,
-      'start_date': '1/1/2018',
-      'frequence': 'H',
-      'solver': 'cbc',
-      'solve_kw': {'tee': True},
-      }
+time_res = 1    # time resolution: [1/h] (percentage of hour) => 0.25 is quarter-hour resolution
+set = {'num_ts': num_ts,            # number of timesteps
+       'time_res': time_res,         # time resolution: [1/h] (percentage of hour) => 0.25 is quarter-hour resolution
+       'rate': 0.01,                 # interest rate (just in case of annuity)
+       'f_invest': num_ts / (8760 / time_res),   # scaling factor for investment costs (just in case annuity is on)
+       # 'f_invest': 1,
+       'start_date': '1/1/2018',     # start of datetimeindex for oemof solph
+       'frequence': 'H',             # time resolution
+       'solver': 'cbc',              # solver
+       'solve_kw': {'tee': True},    # solver kwargs
+       }
 
 network.optimize_investment(settings=set)
 
