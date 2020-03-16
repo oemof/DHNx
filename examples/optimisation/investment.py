cd9e62b9267c18362d5c69816eb16e203cae8007
Johannes Roeder <johannes.roeder@uni-bremen.de>
2020-03-16 17:16:06 +0100

    Update investment example

--- examples/optimisation/investment.py
+++ examples/optimisation/investment.py
@@ -6,7 +6,22 @@ import dhnx
 network = dhnx.network.ThermalNetwork()
 network = network.from_csv_folder('investment_input_2')
 
-network.optimize_investment()
+# some data -> need to be moved somewhere else
+num_ts = 2      # number of timesteps
+time_res = 1    # time resolution: [1/h] (percentage of hour)
+# => 0.25 is quarter-hour resolution
+set = {'num_ts': num_ts,  # number of timesteps
+      'time_res': time_res,
+      'rate': 0.01,
+      'f_invest': num_ts / (8760 / time_res),   # just in case annuity is on
+      # 'f_invest': 1,
+      'start_date': '1/1/2018',
+      'frequence': 'H',
+      'solver': 'cbc',
+      'solve_kw': {'tee': True},
+      }
+
+network.optimize_investment(settings=set)
 
 # Draw network
 static_map = dhnx.plotting.StaticMap(network)
