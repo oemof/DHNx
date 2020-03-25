7ec5659c10840a69d2edf6a71ee8d7b552407d4c
Johannes Roeder <johannes.roeder@uni-bremen.de>
2020-03-25 09:01:00 +0100

    Update example

--- examples/optimisation/investment.py
+++ examples/optimisation/investment.py
@@ -7,7 +7,7 @@ network = dhnx.network.ThermalNetwork()
 network = network.from_csv_folder('investment_input_2/network')
 
 # general optimisation settings
-num_ts = 2      # number of timesteps
+num_ts = 1     # number of timesteps
 time_res = 1    # time resolution: [1/h] (percentage of hour) => 0.25 is quarter-hour resolution
 set = {'num_ts': num_ts,            # number of timesteps
        'time_res': time_res,         # time resolution: [1/h] (percentage of hour) => 0.25 is quarter-hour resolution
@@ -18,6 +18,14 @@ set = {'num_ts': num_ts,            # number of timesteps
        'frequence': 'H',             # time resolution
        'solver': 'cbc',              # solver
        'solve_kw': {'tee': True},    # solver kwargs
+       # optimization types
+       'dhs': 'fix',        # parameter which defines type of optimization. ...
+       # either the heat supply should be via dhs ('fix') (=> all active houses are connected), ...
+       # or is part of the optimization ('optional').
+       'simultaneity': 'global',   # 'global' or 'timeseries' => a single timestep optimization is performed
+       'global_SF': 0.8,           # global simultaneity factor. is only applied, if simultaneity='global'
+       'SF_timeseries': 1,         # scaling factor for heat demand timeseries
+       'SF_1_timeseries': 0.8,     # scaling factor for the first element of the timeseries (bei geoordneter JDL)
        }
 
 invest_opt = dhnx.input_output.load_invest_options('investment_input_2/invest_options')
@@ -46,3 +54,25 @@ network_result.components['edges'] = results_edges.loc[ind_invest_not_zero]
 static_map = dhnx.plotting.StaticMap(network_result)
 static_map.draw(background_map=False)
 plt.show()
+
+
+# TESTING #########################
+
+# results = network.results['optimization']['oemof']
+#
+# label = 'infrastructure_' + 'heat_' + 'heatpipe-milp' + '_' + 'producers-0-forks-0'
+#
+# flow = [x for x in results.keys()
+#         if x[1] is not None
+#         if x[0].label.__str__() == label]
+#
+# results[flow[0]]['sequences']
+
+
+# # maybe slow approach with lambda function
+# df[hp + '.' + 'dir-1'] = df['from_node'] + '-' + df['to_node']
+# df[hp + '.' + 'size-1'] = df[hp + '.' + 'dir-1'].apply(
+#   lambda x: get_invest_val(label_base + x))
+# df[hp + '.' + 'dir-2'] = df['to_node'] + '-' + df['from_node']
+# df[hp + '.' + 'size-2'] = df[hp + '.' + 'dir-2'].apply(
+#   lambda x: get_invest_val(label_base + x))
