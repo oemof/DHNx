46a4154085979793abe504ca497e7cc7092bed2c
Johannes Roeder <johannes.roeder@uni-bremen.de>
2020-04-07 18:39:02 +0200

    Add minimal example for invest optimization

---
+++ examples/optimisation/investment_minimal_example/minimal_invest_example.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import dhnx
+
+
+# Initialize thermal network
+network = dhnx.network.ThermalNetwork()
+network = network.from_csv_folder('twn_data')
+
+# Load investment parameter
+invest_opt = dhnx.input_output.load_invest_options('invest_data')
+
+# Execute investment optimization
+network.optimize_investment(invest_options=invest_opt)
+
+# ####### Postprocessing and Plotting ###########
+# Draw network
+static_map = dhnx.plotting.StaticMap(network)
+static_map.draw(background_map=False)
+plt.title('Given network')
+plt.show()
+
+# get results
+results_edges = network.results.optimization['components']['edges']
+print('*Results*')
+print(results_edges)
+print('')
+print('Objective Value: ',
+      network.results.optimization['oemof_meta']['objective'])
+
+# manually recalculate total costs
+total_costs = (33*3.162 + 15*1 + 18*1 + 18*0.5)*0.5
+print('Costs re-calculation: ', total_costs)
+
+col_size = [x for x in list(results_edges.columns) if '.size' in x]
+col_size = [x for x in col_size if x.split('.')[1] == 'size']
+
+# get indices which are existing or invested
+ind = []
+for hp in col_size:
+    if len(list(results_edges[results_edges[hp] > 0.001].index)) > 0:
+        ind = ind + list(results_edges[results_edges[hp] > 0.001].index)
+
+# select invested edges
+network_result = network
+network_result.components['edges'] = results_edges.loc[ind]
+
+# plot results network
+static_map = dhnx.plotting.StaticMap(network_result)
+static_map.draw(background_map=False)
+plt.title('Optimization result')
+plt.show()
