4b623bd9c7e21ff35b6b3fe585819e128b4ab23a
Johannes Roeder <johannes.roeder@uni-bremen.de>
2020-05-22 21:15:56 +0200

    Add some postprocessing for testing to example

--- examples/optimisation/investment.py
+++ examples/optimisation/investment.py
@@ -29,6 +29,7 @@ set = {'num_ts': num_ts,            # number of timesteps
        'precalc_consumer_connections': True,
        'bidirectional_pipes': True,       # specify whether the distribution lines (from fork to fork) should be bidiretional or not,
                                           # if 'False', then two pipes are build in each direaction, instead of 1 bidirectional pipe
+       'dump_path': 'investment_input_2/'
        }
 
 invest_opt = dhnx.input_output.load_invest_options('investment_input_2/invest_options')
@@ -75,6 +76,41 @@ network_result.components['edges'] = results_edges.loc[ind]
 static_map = dhnx.plotting.StaticMap(network_result)
 static_map.draw(background_map=False)
 plt.show()
+
+# recalculate objective value
+print('Objective: ', network.results['optimization']['oemof_meta']['objective'])
+
+col_res = 'heatpipe-milp.size'
+df_invest = results_edges[results_edges[col_res] > 0].copy()
+df_invest['test_costs'] = (df_invest[col_res] * 0.00959 + 236) * df_invest['length[m]']
+costs = df_invest['test_costs'].sum()
+
+print('Re-calculated costs: ', costs)
+print('')
+
+# recalc heat loss
+# Re-calc heat loss
+results = network.results['optimization']['oemof']
+hp = [x for x in results.keys()
+             if x[1] is None
+             if 'heatpipe' in x[0].label[2]]
+len(hp)
+len(results_edges.index)
+
+label_list = [x[0].label[3] for x in hp]
+heat_loss_list = [results[x]['sequences']['heat_loss'][0].squeeze()
+                       for x in hp]
+heat_loss_total = sum(heat_loss_list)
+
+import pandas as pd
+df_heat_loss = pd.DataFrame([label_list, heat_loss_list]).T
+df_heat_loss.columns = ['label', 'heat_loss']
+
+print('Heat loss nodes: ', df_heat_loss['heat_loss'].sum())
+print('Heat loss recalc: ', results_edges['heat_loss[kW]'].sum())
+
+
+
 
 
 # TESTING #########################
