58cd425f07f6e811597f9413d8448c2f8fc02614
jnnr <jann.launer@rl-institut.de>
2020-08-04 17:23:51 +0200

    Add sketch of hydraulic part

--- dhnx/simulation.py
+++ dhnx/simulation.py
@@ -10,23 +10,63 @@ available from its original location:
 
 SPDX-License-Identifier: MIT
 """
+import networkx as nx
+import numpy as np
+import pandas as pd
 
 from .model import SimulationModel
 
 
-class SimulationModelTespy(SimulationModel):
+class SimulationModelNumpy(SimulationModel):
     r"""
-    Implementation of a simulation model using tespy.
+    Implementation of a simulation model using numpy.
     """
     def __init__(self, thermal_network):
         super().__init__(thermal_network)
         self.results = {}
 
+    def _solve_hydraulic_eqn(self, tolerance = 1e-10):
+        results = {}
+
+        mass_flow_data = self.thermal_network.sequences.consumers.mass_flow
+
+        mass_flow_data.columns = ['consumers-' + m for m in mass_flow_data.columns]
+
+        nx_graph = self.thermal_network.to_nx_graph()
+
+        for t in self.thermal_network.timeindex:
+
+            inc_mat = nx.incidence_matrix(nx_graph, oriented=True).todense()
+
+            mass_flow = {key: 0 for key in nx_graph.nodes()}
+
+            mass_flow.update(dict(mass_flow_data.iloc[t]))
+
+            mass_flow = np.array(list(mass_flow.values()))
+
+            mass_flow[0] = - np.sum(mass_flow[1:])
+
+            x, residuals, rank, s = np.linalg.lstsq(inc_mat, mass_flow, rcond=None)
+
+            assert residuals < tolerance,\
+                f"Residuals {residuals} are larger than tolerance {tolerance}!"
+
+            results.update({t: x})
+
+        results = pd.DataFrame.from_dict(results, orient='index', columns=nx_graph.edges())
+
+        return results
+
+    def _solve_thermal_eqn(self):
+        pass
+
     def setup(self):
         pass
 
     def solve(self):
-        pass
+        mass_flows = self._solve_hydraulic_eqn()
+        print(mass_flows)
+        self._solve_thermal_eqn()
 
     def get_results(self):
         return self.results
@@ -45,7 +85,7 @@ def simulate(thermal_network):
     -------
     results : dict
     """
-    model = SimulationModelTespy(thermal_network)
+    model = SimulationModelNumpy(thermal_network)
 
     model.solve()
 
