29fd7ed32fd7a48b403cf63c8baaad8e0eb4c685
jnnr <jann.launer@rl-institut.de>
2020-09-28 15:51:14 +0200

    Use DiGraph, not MultiDiGraph

--- dhnx/input_output.py
+++ dhnx/input_output.py
@@ -204,6 +204,103 @@ class OSMNetworkImporter(NetworkImporter):
 
         return footprints
 
+    @staticmethod
+    def remove_duplicate_edges(graph):
+
+        graph = nx.DiGraph(graph)
+        return graph
+
+    @staticmethod
+    def remove_self_loops(graph):
+
+        self_loop_edges = list(nx.selfloop_edges(graph))
+
+        graph.remove_edges_from(self_loop_edges)
+
+        return graph
+
+    @staticmethod
+    def graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geometry=True):
+        """
+        Convert a graph into node and/or edge GeoDataFrames. Adapted from osmnx for DiGraph.
+
+        Parameters
+        ----------
+        G : networkx digraph
+        nodes : bool
+            if True, convert graph nodes to a GeoDataFrame and return it
+        edges : bool
+            if True, convert graph edges to a GeoDataFrame and return it
+        node_geometry : bool
+            if True, create a geometry column from node x and y data
+        fill_edge_geometry : bool
+            if True, fill in missing edge geometry fields using origin and
+            destination nodes
+
+        Returns
+        -------
+        GeoDataFrame or tuple
+            gdf_nodes or gdf_edges or both as a tuple
+        """
+        import numpy as np
+        from shapely.geometry import Point
+        from shapely.geometry import LineString
+
+        if not (nodes or edges):
+            raise ValueError('You must request nodes or edges, or both.')
+
+        to_return = []
+
+        if nodes:
+
+            nodes, data = zip(*G.nodes(data=True))
+            gdf_nodes = gpd.GeoDataFrame(list(data), index=nodes)
+            if node_geometry:
+                gdf_nodes['geometry'] = gdf_nodes.apply(lambda row: Point(row['x'], row['y']),
+                                                        axis=1)
+                gdf_nodes.set_geometry('geometry', inplace=True)
+            gdf_nodes.crs = G.graph['crs']
+            gdf_nodes.gdf_name = '{}_nodes'.format(G.graph['name'])
+
+            to_return.append(gdf_nodes)
+
+        if edges:
+
+            # create a list to hold our edges, then loop through each edge in the
+            # graph
+            edges = []
+            for u, v, data in G.edges(data=True):
+
+                # for each edge, add key and all attributes in data dict to the
+                # edge_details
+                edge_details = {'u': u, 'v': v}
+                for attr_key in data:
+                    edge_details[attr_key] = data[attr_key]
+
+                # if edge doesn't already have a geometry attribute, create one now
+                # if fill_edge_geometry==True
+                if 'geometry' not in data:
+                    if fill_edge_geometry:
+                        point_u = Point((G.nodes[u]['x'], G.nodes[u]['y']))
+                        point_v = Point((G.nodes[v]['x'], G.nodes[v]['y']))
+                        edge_details['geometry'] = LineString([point_u, point_v])
+                    else:
+                        edge_details['geometry'] = np.nan
+
+                edges.append(edge_details)
+
+            # create a GeoDataFrame from the list of edges and set the CRS
+            gdf_edges = gpd.GeoDataFrame(edges)
+            gdf_edges.crs = G.graph['crs']
+            gdf_edges.gdf_name = '{}_edges'.format(G.graph['name'])
+
+            to_return.append(gdf_edges)
+
+        if len(to_return) > 1:
+            return tuple(to_return)
+        else:
+            return to_return[0]
+
     def process(self, graph, footprints):
 
         print('Processing...')
@@ -218,14 +315,14 @@ class OSMNetworkImporter(NetworkImporter):
         building_midpoints['y'] = building_midpoints.apply(lambda x: x.geometry.y, 1)
         building_midpoints = building_midpoints[['x', 'y', 'geometry']]
 
-        self_loop_edges = list(nx.selfloop_edges(graph))
+        graph = self.remove_self_loops(graph)
 
-        graph.remove_edges_from(self_loop_edges)
+        graph = self.remove_duplicate_edges(graph)
 
         graph = nx.relabel.convert_node_labels_to_integers(graph)
 
         # get nodes and edges from graph
-        nodes, edges = ox.save_load.graph_to_gdfs(graph)
+        nodes, edges = self.graph_to_gdfs(graph)
 
         nodes = nodes.loc[:, ['x', 'y', 'geometry']]#.reset_index()
         # replace_ids = {v: k for k, v in dict(nodes.loc[:, 'index']).items()}
