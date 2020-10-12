bdf0578673d7f282b29766afb16883a363d4c202
jnnr <jann.launer@rl-institut.de>
2020-10-12 15:31:22 +0200

    Add test that compares benchmark results with dhnx results

---
+++ tests/test_results.py
@@ -0,0 +1,31 @@
+import os
+
+import dhnx
+
+from . import helpers
+
+# define paths
+tests_path = os.path.abspath(os.path.dirname(__file__))
+
+tmp_dir = helpers.extend_basic_path('tmp')
+
+
+def test_tree_simulation():
+
+    input_data = os.path.join(tests_path, '_files', 'tree_network_import')
+
+    expected_results = os.path.join(tests_path, 'tree_network_sim_expected_results', 'sequences')
+
+    results_path = os.path.join(tmp_dir, 'tree_results')
+
+    # Initialize thermal network
+    tnw = dhnx.network.ThermalNetwork()
+
+    # Load data from csv
+    tnw.from_csv_folder(input_data)
+
+    # Create simulation model
+    tnw.simulate(results_dir=results_path)
+
+    # compare with expected results
+    helpers.check_if_csv_dirs_equal(results_path, expected_results)
