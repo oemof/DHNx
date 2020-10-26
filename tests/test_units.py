bf4a3b61eca96d66334dd08a87c2d5a2454491a5
jnnr <jann.launer@rl-institut.de>
2020-10-26 10:59:31 +0100

    Update test assertion

--- tests/test_units.py
+++ tests/test_units.py
@@ -26,7 +26,7 @@ thermal_network = dhnx.network.ThermalNetwork(dir_import)
 def test_add():
     thermal_network.add('Producer', 5, lat=1, lon=1)
 
-    assert thermal_network.components['producers'].loc[5].to_list() == [1., 1., np.nan]
+    assert thermal_network.components['producers'].loc[5].to_list() == [1., 1., "Producer"]
 
 
 def test_remove():
