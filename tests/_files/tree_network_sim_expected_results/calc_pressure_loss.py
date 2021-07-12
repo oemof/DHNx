Fix units (length_m > length, diameter_mm > diameter)

tests/_files/tree_network_sim_expected_results/calc_pressure_loss.py
@@ -83,24 +83,24 @@ for index, node in enumerate(mass_flow_total):
     v[str(index)] = (
         4
         * mass_flow_total[str(node)]
-        / (rho * pi * (pipes["diameter_mm"].iloc[index] / 1000) ** 2)
+        / (rho * pi * (pipes["diameter"].iloc[index] / 1000) ** 2)
     )
 
     # Calculation of Re number
     re[str(index)] = (
-        pipes["diameter_mm"].iloc[index] / 1000 * v[str(index)] * rho / (mu / 1000)
+        pipes["diameter"].iloc[index] / 1000 * v[str(index)] * rho / (mu / 1000)
     )
     # Calculation of lambda with simple approach
     lambda_simp[str(index)] = (
         0.07
         * re[str(index)] ** -0.13
-        * (pipes["diameter_mm"].iloc[index] / 1000) ** -0.14
+        * (pipes["diameter"].iloc[index] / 1000) ** -0.14
     )
     # Calculation of lambda with advanced approach
     lambda_adv[str(index)] = (
         1.325
         / np.log(
-            epsilon / (1000 * 3.7 * (pipes["diameter_mm"].iloc[index] / 1000))
+            epsilon / (1000 * 3.7 * (pipes["diameter"].iloc[index] / 1000))
             + 5.74 / (re[str(index)] ** 0.9)
         )
         ** 2
@@ -109,9 +109,9 @@ for index, node in enumerate(mass_flow_total):
     dp_diss[str(index)] = (
         lambda_simp[str(index)]
         * rho
-        * pipes["length_m"].iloc[index]
+        * pipes["length"].iloc[index]
         * v[str(index)] ** 2
-        / (2 * (pipes["diameter_mm"].iloc[index] / 1000))
+        / (2 * (pipes["diameter"].iloc[index] / 1000))
     )
     # Calculate local pressure losses resulted from separating Tee (T-Stück) -> i - inlet
     # Localized Pressure losses only occur in the outlet pipes of the tee separator

tests/_files/tree_network_sim_expected_results/calc_temp_drop.py
@@ -27,7 +27,7 @@ def read_data(input_value):
 
 # Read input data for every csv component
 pipes = read_data("pipes")
-temp_drop = pd.read_csv(input_data + "/sequences/consumers-temperature_drop.csv")
+temp_drop = pd.read_csv(input_data + "/sequences/consumers-delta_temp_drop.csv")
 mass_flow = pd.read_csv(input_data + "/sequences/consumers-mass_flow.csv")
 
 # Constants for calculation
@@ -75,11 +75,11 @@ def calc_temp_heat_loss(t_in, pos):
             Temperature leaving the pipe
     """
     t_out = t_env + (t_in - t_env) * np.exp(
-        -pipes["heat_transfer_coefficient_W/mK"].iloc[pos]
+        -pipes["heat_transfer_coefficient"].iloc[pos]
         * pi
-        * pipes["diameter_mm"].iloc[pos]
+        * pipes["diameter"].iloc[pos]
         / 1000
-        * pipes["length_m"].iloc[pos]
+        * pipes["length"].iloc[pos]
         / (c * mass_flow_total[str(pos)])
     )
     return t_out