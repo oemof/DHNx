cf499fe046603561630368294c32bda039afd236
Johannes Roeder <johannes.roeder@uni-bremen.de>
2021-07-28 14:42:20 +0200

    Uncomment export of example again

--- examples/import_osmnx/import_osmnx.py
+++ examples/import_osmnx/import_osmnx.py
@@ -1,4 +1,4 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 from dhnx.input_output import OSMNetworkImporter
 from dhnx.plotting import StaticMap
@@ -21,6 +21,6 @@ static_map = StaticMap(tnw)
 
 static_map.draw(background_map=False)
 
-# plt.savefig('static_map_wo_background.png')
+plt.savefig('static_map_wo_background.png')
 
-# tnw.to_csv_folder('osm_network')
+tnw.to_csv_folder('osm_network')
