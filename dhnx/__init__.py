4a2bab66e03d4359c3ebd7aa59a92983ae166c50
Johannes Roeder <johannes.roeder@uni-bremen.de>
2021-07-29 10:53:25 +0200

    Import modules in init and ignore flake8 warning

--- dhnx/__init__.py
+++ dhnx/__init__.py
@@ -1,1 +1,16 @@
 __version__ = "0.0.3dev0"
+
+from . import dhn_from_osm  # noqa: F401
+from . import graph  # noqa: F401
+from . import helpers  # noqa: F401
+from . import input_output  # noqa: F401
+from . import model  # noqa: F401
+from . import network  # noqa: F401
+from . import plotting  # noqa: F401
+from . import simulation  # noqa: F401
+from .gistools import connect_points  # noqa: F401
+from .gistools import geometry_operations  # noqa: F401
+from .optimization import add_components  # noqa: F401
+from .optimization import dhs_nodes  # noqa: F401
+from .optimization import oemof_heatpipe  # noqa: F401
+from .optimization import optimization_models  # noqa: F401
---
+++ dhnx/optimization/__init__.py
@@ -0,0 +1,4 @@
+from . import add_components  # noqa: F401
+from . import dhs_nodes  # noqa: F401
+from . import oemof_heatpipe  # noqa: F401
+from . import optimization_models  # noqa: F401
