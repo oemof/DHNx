975ef8c68af989b96743afa6197c2196b537ac9c
Johannes Roeder <johannes.roeder@uni-bremen.de>
2021-07-29 10:18:45 +0200

    Fix flake8 remove unused imports

--- dhnx/__init__.py
+++ dhnx/__init__.py
@@ -1,14 +1,1 @@
 __version__ = "0.0.3dev0"
-
-from . import input_output
-from . import network
-from . import plotting
-from . import model
-from . import simulation
-from . import dhn_from_osm
-from .gistools import connect_points
-from .gistools import geometry_operations
-from .optimization import add_components
-from .optimization import dhs_nodes
-from .optimization import oemof_heatpipe
-from .optimization import optimization_models
--- dhnx/optimization/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import add_components
-from . import dhs_nodes
-from . import oemof_heatpipe
-from . import optimization_models
