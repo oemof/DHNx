3dd383e4b3c9bff4ac9a5a44aff41ce5dfe75d5a
Johannes Roeder <johannes.roeder@uni-bremen.de>
2022-04-20 11:31:04 +0200

    Update inits

--- dhnx/optimization/__init__.py
+++ dhnx/optimization/__init__.py
@@ -2,3 +2,4 @@ from . import add_components  # noqa: F401
 from . import dhs_nodes  # noqa: F401
 from . import oemof_heatpipe  # noqa: F401
 from . import optimization_models  # noqa: F401
+from . import precalc_hydraulic  # noqa: F401
